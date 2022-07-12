0ab816e87ccf8d9495f6cd9ec5ca17bcd0742967
AyzeLYC <35877385+AyzeGamingFR@users.noreply.github.com>
2022-07-12 12:20:31 +0200

    Created main.cpp

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <fstream>
+#include <std>
+#include <string>
+
+namespace main {
+    
+    string config = {"blockchain": {"maxBlockSize": 0}, "networking": {"maxConnections": 128}, "wallet": {"walletPassword": "", "walletPrivateKeys": [], "walletPublicKeys": []}};
+    
+    unsigned int blockchain() {
+        
+        
+        
+    };
+    unsigned int hashing() {
+        
+        
+        
+    };
+    unsigned int networking() {
+        
+        
+        
+    };
+    unsigned int wallet() {
+        
+        
+        
+    };
+    
+};
