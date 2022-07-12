0261d6bf3c461ecd5e3adf0e8fe079f50fddb14d
AyzeLYC <35877385+AyzeGamingFR@users.noreply.github.com>
2022-07-12 12:33:31 +0200

    Updated main.cpp

--- main.cpp
+++ main.cpp
@@ -8,22 +8,110 @@ namespace main {
     
     unsigned int blockchain() {
         
-        
+        unsigned int createBlock() {
+            
+            
+            
+        };
+        unsigned int createBlockchain() {
+            
+            
+            
+        };
         
     };
     unsigned int hashing() {
         
+        unsigned int scrypt(string datas, unsigned int repetitions, unsigned int x1, unsigned int x2) {
+            
+            for (repetitions > 0) {
+                
+                repetitions -= 1;
+                datas = hash(datas);
+                
+            };
+            unsigned int hash(string datas) {
+                
+                
+                
+            };
+            
+        };
+        unsigned int sha256(string datas) {
+            
+            
+            
+        };
+        unsigned int sha512(string datas) {
+            
+            
+            
+        };
+        unsigned int x11(string datas) {
+            
+            
+            
+        };
+        unsigned int x12(string datas) {
+            
+            
+            
+        };
+        unsigned int x16r(string datas) {
+            
+            
+            
+        };
+        unsigned int x16s(string datas) {
+            
+            
+            
+        };
         
+        unsigned int antiAsics(string datas) {
+            
+            
+            
+        };
         
     };
     unsigned int networking() {
         
+        unsigned int addPeer(string address, unsigned int port, unsigned int blockchainId) {
+            
+            
+            
+        };
+        unsigned int connectToPeer(string address, unsigned int port, unsigned int blockchainId) {
+            
+            
+            
+        };
+        unsigned int findPeers(unsigned int port, unsigned int blockchainId) {
+            
+            
+            
+        };
         
+        unsigned int remoteCommandsProtocol() {
+            
+            
+            
+        };
         
     };
     unsigned int wallet() {
         
-        
+        unsigned int generatePrivateKey() {
+            
+            
+            
+        };
+        unsigned int derivatePrivateKey(string datas) {
+            
+            
+            
+        };
         
     };
     
